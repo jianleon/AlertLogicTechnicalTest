033fc256628f5972806dfa917643f81fd5b888d2
Jimmy León <jimmy.leon@apostar.com.co>
2018-11-17 12:05:45 -0500

    Primes implementation in Python 3

---
+++ primes.py
@@ -0,0 +1,39 @@
+""" Function to return prime numbers between 2 and 100 """
+
+
+def is_prime(number):
+    """ Asserts if number is prime.
+    
+    Parameter
+    ---------
+
+    number: int
+        Number to assert
+
+    """
+    for num in range(2, number):
+        if number % num == 0:
+            return False
+    return True
+
+
+def get_primes(limit):
+    """ Return a list with the primes numbers between 2 and 'limit'.
+        
+        Parameter
+        ---------
+
+        limit: int
+            Limit number
+
+        Return
+        ------
+
+        primes_list: list(int)
+            List with primes number in given limit.
+
+    """
+    primes_list = [number for number in range(2, limit + 1) if is_prime(number)]
+    return(primes_list)
+
+print(get_primes(100))
